037573de21e4ff8c02e1c6d0e74b3c274653fdae
pujanov69 <pujanov69@gmail.com>
2020-12-28 12:47:45 +0545

    architecturized app

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,11 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+// import icons from '../img/icons.svg'; //Parcel 1
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +19,31 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+    //1. Loading Recipe
+
+    await model.loadRecipe(id);
+
+    //2) Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev =>
+  window.addEventListener(ev, controlRecipes)
+);
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
